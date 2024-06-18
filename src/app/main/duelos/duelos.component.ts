a17ff62bcced910ca179ca06e2a7f8159e272629
Angel Olazabal <angeljoq26@gmail.com>
2024-06-17 20:57:34 -0500

    Correciones del boton para la ronda 2 y detalles

--- src/app/main/duelos/duelos.component.ts
+++ src/app/main/duelos/duelos.component.ts
@@ -13,5 +13,23 @@ export class DuelosComponent {
 
   constructor(public sharedService: SharedService){}
 
+  ////ESTO AUN NO LO USAMOS, PARA PROXIMOS AVANCES.
+
+  // getNamesInPairs(): string[][][] { 
+  //   const pairs = [];
+  //   for (let j = 0; j < this.sharedService.winnersTotal.length; j++) {
+  //     const roundPairs = [];
+  //     for (let i = 0; i < this.sharedService.winnersTotal[j].length; i += 2) {
+  //       roundPairs.push([this.sharedService.winnersTotal[j][i], this.sharedService.winnersTotal[j][i + 1]]);
+  //     }
+  //     pairs.push(roundPairs);
+  //   }
+  //   return pairs;
+  // }
+
+  
   
 }
+
+
+

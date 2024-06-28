1c445520eb81d2bfa817dc60b7702fd14653b82c
Flagon92 <maxmurillovargas@gmail.com>
2024-06-27 21:27:52 -0500

    Componente Bracket Funcional
    Falta incorporar aspecto visual

--- src/app/main/bracket/bracket.component.ts
+++ src/app/main/bracket/bracket.component.ts
@@ -12,29 +12,11 @@ import { SharedService } from '../../shared.service';
 })
 export class BracketComponent {
 
-  duelos: {
-    nombreEquipo: string;
-    procedencia: string;
-    representante: string;
-    email: string;
-    telefono: string;
-  }[][] = [];
-
   constructor(public sharedService: SharedService) { }
 
   ngOnInit(): void {
-    this.sharedService.getDuelos().subscribe((data) => {
-      this.duelos = this.chunkArray(data, 2);
-    });
+    this.sharedService.setJugadores();
+    this.sharedService.addPartidos();
   }
-
-  private chunkArray(myArray: any[], chunk_size: number) {
-    const results = [];
-    while (myArray.length) {
-      results.push(myArray.splice(0, chunk_size));
-    }
-    return results;
-  }
-
 
 }

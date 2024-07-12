380ba4d7432a392ca4e14410a2356b7de4390b55
Flagon92 <maxmurillovargas@gmail.com>
2024-07-12 03:16:06 -0500

    Pasando logica de Bracket.ts al shared.service

--- src/app/main/bracket/bracket.component.ts
+++ src/app/main/bracket/bracket.component.ts
@@ -13,149 +13,78 @@ import { SharedService } from '../../shared.service';
 
 export class BracketComponent {
 
-  partidos: {
-    nombreEquipo: string,
-    procedencia: string,
-    representante: string,
-    email: string,
-    telefono: string
-  }[] = [];
+  constructor(public sharedService: SharedService) { }
 
   sortearDeshabilitado: boolean = true;
 
-  i: any;
+  ngOnInit() {
+    this.sharedService.inicializarDuelos();
 
-  constructor(private sharedService: SharedService) { }
+  }
 
   barajar(): void {
-    this.sharedService.randomizarEquipos();
-    this.partidos = this.sharedService.resultadosArreglo;
-    if (this.partidos.length >= 4) {
-      this.sortearDeshabilitado = false;
-    } else {
-      // Minimo de equipos antes de permitir barajar
-      console.error('Ingrese al menos cuatro equipos.');
-    }
+    this.sharedService.barajar();
+
+    this.sortearDeshabilitado = false;
+  }
+
+  get partidos() {
+    return this.sharedService.partidos;
+  }
+
+  get ganadoresRonda1() {
+    return this.sharedService.ganadoresRonda1;
+  }
+
+  get ganadoresRonda2() {
+    return this.sharedService.ganadoresRonda2;
+  }
+
+  get ganadoresRonda3() {
+    return this.sharedService.ganadoresRonda3;
+  }
+
+  get ganadoresRonda4() {
+    return this.sharedService.ganadoresRonda4;
+  }
+
+  get duelosDecididosRonda1() {
+    return this.sharedService.duelosDecididosRonda1;
+  }
+
+  get duelosDecididosRonda2() {
+    return this.sharedService.duelosDecididosRonda2;
+  }
+
+  get duelosDecididosRonda3() {
+    return this.sharedService.duelosDecididosRonda3;
+  }
+
+  get duelosDecididosRonda4() {
+    return this.sharedService.duelosDecididosRonda4;
   }
 
   participantesNecesarios(n: number): boolean {
-    return (n & (n - 1)) === 0 && n > 0;
+    return this.sharedService.participantesNecesarios(n);
   }
-
 
   getDuelIndexes(numGanadores: number): number[] {
-    const duelIndexes = [];
-    for (let i = 0; i < numGanadores; i += 2) {
-      duelIndexes.push(i);
-    }
-    return duelIndexes;
+    return this.sharedService.getDuelIndexes(numGanadores);
   }
 
-
-  ngOnInit() {
-    this.inicializarDuelos();
-  }
-
-  ganadoresRonda1: any[] = []; // Array para los ganadores de la ronda 1
-  ganadoresRonda2: any[] = []; // Array para los ganadores de la ronda 2
-  ganadoresRonda3: any[] = []; // Array para los ganadores de la ronda 3
-  ganadoresRonda4: any[] = []; // Array para los ganadores de la ronda 4
-
-
-  duelosDecididosRonda1: boolean[] = []; // Array de booleanos para la ronda 1
-  duelosDecididosRonda2: boolean[] = []; // Array de booleanos para la ronda 2
-  duelosDecididosRonda3: boolean[] = []; // Array de booleanos para la ronda 3
-  duelosDecididosRonda4: boolean[] = []; // Array de booleanos para la ronda 4
-
-  inicializarDuelos(): void {
-    const numDuelosRonda1 = Math.ceil(this.partidos.length / 2);
-    this.duelosDecididosRonda1 = new Array(numDuelosRonda1).fill(false);
-
-    const numDuelosRonda2 = Math.ceil(numDuelosRonda1 / 2);
-    this.duelosDecididosRonda2 = new Array(numDuelosRonda2).fill(false);
-
-    const numDuelosRonda3 = Math.ceil(numDuelosRonda2 / 2);
-    this.duelosDecididosRonda3 = new Array(numDuelosRonda3).fill(false);
-
-    const numDuelosRonda4 = Math.ceil(numDuelosRonda3 / 2);
-    this.duelosDecididosRonda4 = new Array(numDuelosRonda4).fill(false);
-
-    const numDuelosRonda5 = Math.ceil(numDuelosRonda3 / 2);
-    this.duelosDecididosRonda4 = new Array(numDuelosRonda4).fill(false);
-  }
-
-
-  // Función para agregar ganador en la ronda 1
   agregarGanadorRonda1(index: number, duelIndex: number): void {
-    if (index >= 0 && index < this.partidos.length) {
-      const ganador = { ...this.partidos[index] };
-      this.ganadoresRonda1.push(ganador);
-      this.duelosDecididosRonda1[duelIndex] = true;
-      console.log(this.ganadoresRonda1);
-    } else {
-      console.error(`Índice inválido ${index}. El tamaño del array es ${this.partidos.length}`);
-    }
+    this.sharedService.agregarGanadorRonda1(index, duelIndex);
   }
-  // Función para agregar ganador en la ronda 2
+
   agregarGanadorRonda2(index: number, duelIndex: number): void {
-    if (index >= 0 && index < this.ganadoresRonda1.length) {
-      const ganador = { ...this.ganadoresRonda1[index] };
-      this.ganadoresRonda2.push(ganador);
-      this.duelosDecididosRonda2[duelIndex] = true;
-      console.log(this.ganadoresRonda2);
-    } else {
-      console.error(`Índice inválido ${index}. El tamaño del array es ${this.ganadoresRonda1.length}`);
-    }
+    this.sharedService.agregarGanadorRonda2(index, duelIndex);
   }
 
-
-  // Función para agregar ganador en la ronda 3
   agregarGanadorRonda3(index: number, duelIndex: number): void {
-    if (index >= 0 && index < this.ganadoresRonda2.length) {
-      const ganador = this.ganadoresRonda2[index];
-      this.ganadoresRonda3.push(ganador);
-      this.duelosDecididosRonda3[duelIndex] = true; // Desactivar el duelo correspondiente
-
-      // Otras operaciones si es necesario
-
-      console.log('Ganadores Ronda 3:', this.ganadoresRonda3);
-      console.log('Duelos decididos Ronda 3:', this.duelosDecididosRonda3);
-
-      // Verificar si se debe mostrar la ronda 4
-      if (this.ganadoresRonda3.length > 1) {
-        // Mostrar la ronda 4 si hay más de un ganador en la ronda 3
-      } else {
-        // No hacer nada si solo hay un ganador (el torneo ha terminado)
-      }
-    } else {
-      console.error(`Índice inválido ${index}. El tamaño del array es ${this.ganadoresRonda2.length}`);
-    }
+    this.sharedService.agregarGanadorRonda3(index, duelIndex);
   }
 
-
-  // Función para agregar ganador en la ronda 4
   agregarGanadorRonda4(index: number, duelIndex: number): void {
-    if (index >= 0 && index < this.ganadoresRonda3.length) {
-      const ganador = this.ganadoresRonda3[index];
-      this.ganadoresRonda4.push(ganador);
-      this.duelosDecididosRonda4[duelIndex] = true; // Desactivar el duelo correspondiente
-
-      // Otras operaciones si es necesario
-
-      console.log('Ganadores Ronda 4:', this.ganadoresRonda4);
-      console.log('Duelos decididos Ronda 4:', this.duelosDecididosRonda4);
-
-      // Verificar si se debe mostrar la ronda 5
-      if (this.ganadoresRonda4.length > 1) {
-        // Mostrar la ronda 4 si hay más de un ganador en la ronda 3
-      } else {
-        // No hacer nada si solo hay un ganador (el torneo ha terminado)
-      }
-    } else {
-      console.error(`Índice inválido ${index}. El tamaño del array es ${this.ganadoresRonda3.length}`);
-    }
+    this.sharedService.agregarGanadorRonda4(index, duelIndex);
   }
-
-
-
 }
